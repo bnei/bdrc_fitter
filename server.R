36207b529fba7ecda17ede312d5c4047f9f51ba2
bnei <briannei@gmail.com>
2025-05-21 19:38:00 -0700

    Housekeeping, no new functionality

--- server.R
+++ server.R
@@ -14,11 +14,11 @@ server <- function(input, output) {
       includeMarkdown("./README.md")
     }
   })
-  
+
   output$formula <- renderUI({
     withMathJax("$$Q = a(H-c)^b$$")
   })
-  
+
   rc_points <- reactive({
     req(input$points_upload)
     read_csv(input$points_upload$datapath)
@@ -31,28 +31,28 @@ server <- function(input, output) {
   curve.fit <- eventReactive(input$triggerFit, {
     plm(Q ~ H, rc_points())
   })
-  
+
   curve_plot <- reactive({
     curve.fit()$rating_curve
   })
-  
+
   parameters <- reactive({
     fitted_rc <- curve.fit()
-    
+
     list(
       a = fitted_rc$param_summary$median[[1]],
       b = fitted_rc$param_summary$median[[2]],
       c = fitted_rc$param_summary$median[[3]]
     )
   })
-  
+
   output_params <- reactive({
     params <- parameters()
-    
+
     a <- round(params$a, 4)
     b <- round(params$b, 4)
     c <- round(params$c, 4)
-    
+
     tribble(
       ~Parameter, ~Value,
       "a", a,
@@ -60,14 +60,14 @@ server <- function(input, output) {
       "c", c
     )
   })
-  
+
   output$rating_curve <- renderPlot(
     ggplot() +
       geom_point(data = rc_points(), aes(Q, H)) +
       geom_line(data = curve_plot(), aes(median, h)) +
       geom_line(data = curve_plot(), aes(upper, h), linetype = "dotted") +
       geom_line(data = curve_plot(), aes(lower, h), linetype = "dotted"))
-  
+
   output$parameter_table <- renderTable(
     output_params()
   )
--- ui.R
+++ ui.R
@@ -5,39 +5,38 @@ library(bdrc)
 library(bslib)
 
 ui <- fluidPage(
-  
+
   theme = shinytheme("yeti"),
   tags$head(HTML("<title>Bayesian Discharge Rating Curve Fitter</title>")),
-  
+
   titlePanel("Bayesian Discharge Rating Curve Fitter"),
-  
+
   mainPanel(
-    
+
     tabsetPanel(
-      
+
       tabPanel("Read Me", htmlOutput("README")),
-      
+
       tabPanel("Curve Fitter",
-               sidebarLayout(
-                 
-                 sidebarPanel(width = 3, 
-                              fileInput("points_upload", 
-                                        "Upload Stage-Discharge Points:", 
-                                        accept = ".csv"),
-                              
-                              actionButton("triggerFit", 
-                                           "Fit Curve (CLICK ONCE AND WAIT)", 
-                                           class = "btn btn-primary"),
-                              
-                              tableOutput("rc_table"),
-                              ),
-                 
-                 mainPanel(
-                   uiOutput("formula"),
-                   br(),
-                   plotOutput("rating_curve"),
-                   br(),
-                   tableOutput("parameter_table")))
+        sidebarLayout(
+          sidebarPanel(
+            width = 3,
+            fileInput("points_upload",
+                      "Upload Stage-Discharge Points:",
+                      accept = ".csv"),
+            actionButton("triggerFit",
+                         "Fit Curve (CLICK ONCE AND WAIT)",
+                         class = "btn btn-primary"),
+            tableOutput("rc_table"),
+          ),
+          mainPanel(
+            uiOutput("formula"),
+            br(),
+            plotOutput("rating_curve"),
+            br(),
+            tableOutput("parameter_table")
+          )
+        )
       )
     )
   )
